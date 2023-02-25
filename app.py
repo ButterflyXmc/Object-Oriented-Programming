1c4359e024a95e158eda45eddc3ac7ce2a588ef7
mahic <mahic@domain.com>
2023-02-25 14:08:21 -0700

    Classes

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ class Employee:
         self.pay = pay
         self.location = None
     
+    # This fuction prints first and last name
     def say_name(self):
         print(self.first_name, self.last_name)
 
-    # def pay_rise(self, amount):
-    #     self.pay = 
-    
+    # This fucntion takes the default pay and add a certain amount
+    def pay_rise(self, amount):
+        self.pay = self.pay + amount
     
 
 # 2. Using it - we call the class name with a list of argument
@@ -28,7 +29,17 @@ emp_three = Employee("Eric", "Doe", 20)
 # 3.can use this to print
 emp_one.say_name()
 emp_two.say_name()
+
+# printig the pay
+# prints the pay for the emp
+print(emp_one.pay)
 print(emp_three.pay)
+
+# adds 2 to emp_one's pay
+emp_one.pay_rise(2)
+
+# prints emp_one's added amount
+print(emp_one.pay)
 
 # if you want to convert to dictionary
 # print(emp_one.__dict__)
@@ -44,19 +55,26 @@ print(emp_three.pay)
 # .... kind of like a prop function in vue...
 
 #* class - the key word
-#* code needed for an employee in our system
 #* rn we created an empty table
 #class Employee:
 #   pass
+
 # * defining an init function that takes any argument
 # every class fucntion needs to have self. Python will take care of the self. Anything beyond self, we have to take care.
 #  def __init__(self)
-# * pass objects (f_name,l_name)
+
+# * pass objects 
+# (f_name,l_name)
 
 #* using the class
 # - we call the class name with a list of argument
 
-# # now each emplyee I create, they'll always defalut to 15 
-        #* self.pay = 15
-# If i want to overwrite it, i need to define it in the bracket
-#*def __init__(self, f_name, l_name, pay=15):
+#* with the code below, each emplyee I create, they'll always defalut to 15 
+# self.pay = 15
+
+#* If I want to overwrite it, I need to define it in the bracket
+# def __init__(self, f_name, l_name, pay=15):
+
+#*THE reason why we use classes
+# With OOP, instead of writing a program, you create classes. A class contains both data and functions
+# When you want to create something in memory, you create an object, which is an instance of that class.
