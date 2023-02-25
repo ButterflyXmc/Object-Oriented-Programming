202cf63c9669d4561a7077dad1732bca5375f4a2
mahic <mahic@domain.com>
2023-02-25 13:40:20 -0700

    simple example

--- app.py
+++ app.py
@@ -1,5 +1,42 @@
-# class - the key word
-# code needed for an employee in our system
-# rn we created an empty table
+# this is just us defining a structure
+# We haven't done anything with it yet, its just a structure!!!
 class Employee:
-    pass
+    def __init__(self, f_name, l_name):
+        self.first_name = f_name
+        self.last_name = l_name
+    
+    def say_name(self):
+        print(self.first_name, self.last_name)
+
+# Using it - we call the class name with a list of argument
+emp_one = Employee("John", "Doe")
+emp_two = Employee("Jane", "Doe")
+
+print(emp_one.first_name, emp_one.last_name)
+print(emp_two.first_name, emp_two.last_name)
+
+# if you want to convert to dictionary
+print(emp_one.__dict__)
+
+
+
+
+
+
+# !......................................NOTES..............................................
+# this is just us defining a structure
+# We havent done anything with it yet, its just a structure!!!
+# .... kind of like a prop function in vue...
+
+#* class - the key word
+#* code needed for an employee in our system
+#* rn we created an empty table
+#class Employee:
+#   pass
+# * defining an init function that takes any argument
+# every class fucntion needs to have self. Python will take care of the self. Anything beyond self, we have to take care.
+#  def __init__(self)
+# * pass objects (f_name,l_name)
+
+#* using the class
+# - we call the class name with a list of argument
